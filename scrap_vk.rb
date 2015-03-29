8b70f657b8c190c64cd3e6a0e8f93ba3aaac23eb
SerVac <vacsaman@gmail.com>
2015-03-29 06:11:50 -0700

    origin

--- scrap_vk.rb
+++ scrap_vk.rb
@@ -25,8 +25,10 @@ class ScrapVK
     if (!ARGV.empty?)
       @login = ARGV.shift
       @pass = ARGV.shift
-      @start_page += ARGV.shift
+      #{@start_page} += ARGV.shift
     end
+
+    @start_page = "/album19839792_000"
 
     # @session = Capybara::Session.new :webkit
     Capybara.default_driver = :webkit
@@ -57,60 +59,134 @@ class ScrapVK
       fill_in('quick_pass', with: @pass)
     end
 
-    @doc = Nokogiri::HTML.parse(page.html)
-    @doc.css('img').each do |i|
-      puts i
-    end
-
     button = find('#quick_login_button')
-    button.click
+    button.click()
 
   end
-
 
   def imgSaver
     system 'mkdir', '-p', SAVE_FOLDER_NAME
 
-    putsCurrUrl()
-    @doc = Nokogiri::HTML.parse(page.html)
+    img_total = find('.summary').text
+    img_total = /\s(\d*)\s/.match(img_total)[0].to_i
+    puts "num= "+img_total.to_s
 
-    @doc.css('img').each do |i|
-      saveImgNode(i)
-    end
+    img_start = first(:css, 'div.photo_row > a')
+    img_start.click()
+
+    img_list = []
+    ignore_list = []
+    css_photo = 'a#pv_photo'
+    css_origin = 'a#pv_open_original'
+    file_name = ''
+
+    count_retry = 0
+    begin
+      puts ""
+      puts "--next"
+
+      imgs_box = find('div#pv_box')
+      if (imgs_box.assert_selector(css_photo))
+
+        img_link = imgs_box.find(css_photo)
+        puts img_link['href']
+
+        begin
+          if (imgs_box.assert_selector(css_origin))
+            file_name = saveByLink(imgs_box.find(css_origin)['href'])
+          else
+            file_name = saveByImgNode(img_link.find('img'))
+          end
+          img_list.push(file_name)
+          puts " file added: "+file_name
+          img_link.click()
+          puts " clicked"
+
+          count_retry=0
+
+        rescue StandardError => e
+          # file_name = /(?<=\({1})(.+)/.match(e.to_s).to_s
+          # puts " error fname= "+@curr_file_name
+          if (!ignore_list.include?(@curr_file_name))
+            ignore_list.push(@curr_file_name)
+            puts " :ignore"
+          else
+            count_retry+=1
+            puts " :retry "+count_retry.to_s
+            if (count_retry > 3)
+              count_retry = 0
+              puts " :clear "+count_retry.to_s
+              puts " :break"
+              break
+            end
+          end
+
+          img_link.click()
+          sleep 2
+        end
+
+      else
+        puts "break !"
+        break
+      end
+
+    end while img_list.size < img_total
+    puts img_list.to_s
+    puts "- the '"+img_list.size.to_s+"' images from album are '"+@start_page+"' saved! -"
 
 =begin
-      images = @doc.css('img')
-      i = images.first
-      saveImgNode(i)
+    img_total = find('.summary').text
+    img_total = /\s(\d*)\s/.match(img_total)[0].to_i
+    # puts "num= "+img_total.to_s
+
+    img_pool = []
+    @doc = Nokogiri::HTML.parse(page.html)
+    @doc.css('div.photo_row').each do |i|
+      img_pool.push(i)
+    end
+    puts img_pool.count
+    i = img_pool[0]
+    find('#'+i['id']).click()
 =end
 
   end
 
+  def saveByImgNode(img_node)
+    puts "-Save img: "+img_node.to_s
+    return saveByLink(img_node['src'])
+    # img_name = /(?<=\/{1})(\w*.(jpg|png|gif|bmp){1})/.match(src).to_s
+  end
 
-  def saveImgNode(imgNode)
-    puts "-saveImg: "+imgNode.to_s
-    src = imgNode['src']
-    puts " src: "+src
+  def saveByLink(link)
+    puts "-Save file by link: "+link
+    file_name = nil
+    if (!link.nil?)
+      if (!/(#{PROTOCOL_PREAMBL})/.match(link).nil?)
+        file_name = File.basename(link)
+        @curr_file_name = file_name
+        puts " file name: "+file_name
 
-    if (!src.nil?)
-      # img_name = /(?<=\/{1})(\w*.(jpg|png|gif|bmp){1})/.match(src).to_s
-
-      if (!/(#{PROTOCOL_PREAMBL})/.match(src).nil?)
-        # uri = /(\/{1}\w*.{1}(jpg|png|gif|bmp){1}$)/.match(src).to_s
-        uri = File.basename(src)
-        puts " img name: "+uri
-
-        save_as = SAVE_FOLDER_NAME+uri
-        puts " save as: "+save_as
-
-        if (!uri.nil? && !uri.empty?)
-          File.open(save_as, 'wb') { |f| f.write(open(src).read) }
+        save_as = SAVE_FOLDER_NAME+file_name
+        if (!File.file?(save_as))
+          File.open(save_as, 'wb') { |f| f.write(open(link).read) }
+          puts " file save as path: "+save_as
+          return file_name.to_s
         end
-      else
-        puts " !img don't consist http!"
-      end
 
+        puts " :file '"+file_name+"' is exist !"
+      else
+        puts " :save link is not consist 'http' !"
+      end
+    else
+      " :save link is empty!"
     end
+
+
+    # rescue SystemCallError
+    #   " :file is exist" if file_name.nil?
+
+    raise " :file ("+file_name+") is exist"
+    return nil
   end
 
   def putsCurrUrl
@@ -118,59 +194,3 @@ class ScrapVK
   end
 
 end
-
-
-=begin
-    enter_page.links.each do |link|
-      text = link.text.strip
-      next unless text.length > 0
-      puts text
-    end
-=end
-
-=begin
-    session = Capybara::Session.new(:webkit)
-    session.visit "http://www.amberbit.com"
-
-    if session.has_content?("Ruby on Rails web development")
-      puts "All shiny, captain!"
-    else
-      puts ":( no tagline fonud, possibly something's broken"
-      exit(-1)
-    end
-=end
-
-=begin
-test = "https://pp.vk.me/c622728/v622728792/23feb/GS3X2bw8DG4.jpg"
-n = /(http)/.match(test)
-if (!n.nil?)
-  m = /(\/{1}\w*.{1}(jpg|png|gif|bmp){1}$)/.match(test)
-  puts m.to_s
-end
-=end
-
-=begin
-    all(:css, 'img').each do |image|
-      puts image.native.children[1]
-    end
-=end
-# page.has_contest?('')
-
-# current_path.should == "/feed"
-=begin
-    puts @session.find('#quick_email')
-    puts @session.find_field('quick_email')
-
-    @session.within('form#quick_login_form') do
-      @session.fill_in('quick_email',  :with => @login)
-      @session.fill_in('quick_pass', with: @pass)
-      # choose('A Radio Button')
-      # check('A Checkbox')
-      # uncheck('A Checkbox')
-      # attach_file('Image', '/path/to/image.jpg')
-      # select('Option', :from => 'Select Box')
-      end
-
-      # click_link 'Sign in'
-    end
-=end
